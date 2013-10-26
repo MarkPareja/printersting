4eaf1eadfc366992ebcae2a237e638c85737450c
Mark Pareja <me@markpareja.com>
2013-10-26 01:30:11 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Printersting::Application.config.secret_key_base = '9e30a39a151ffa2a0f2827408f096a2c01c2eb2c6455ccdbbd23ffce8b1c36580f588b11f24c4b453ab861bda5c27cde57e27500e27c136f4c2443713191f475'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Printersting::Application.config.session_store :cookie_store, key: '_printersting_session'
