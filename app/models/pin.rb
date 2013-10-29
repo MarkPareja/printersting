2b3f7d775657881e6ab0149fab4a61549c78ce32
Mark Pareja <markp@godaddy.com>
2013-10-29 02:12:36 -0700

    Updated Yml

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,8 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image,
-		:styles => { :medium => "300x300>", :thumb => "100x100>" },
 		:storage => :dropbox,
    		:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    	:dropbox_options => {...} 
+    	:dropbox_options => {...},
+    	:styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
